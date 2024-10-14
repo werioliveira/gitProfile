72108a5acdfdb2cc24235271ff4779163caa01d5
Weri Oliveira <werioliveira@hotmail.com>
2024-10-14 09:21:29 -0300

    update design git profile and searchs

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,11 +4,13 @@ async function index(req,res){
     var latestSearch = []
     let result = await getLatest()
     latestSearch.push(result)
-    return res.render('index',{latest: latestSearch[0]})
+    return res.render('index',{latest: latestSearch[0], user: [],repos: [] })
 }
 async function getProfile(req,res){
     const user = 'users'+'/'+req.body.user
     const repos = 'users'+'/'+req.body.user+'/repos'
+    var latestSearch = []
+
     db.run(`INSERT INTO latestSearch(search) VALUES(?)`, req.body.user, function(err) {
         if (err) {
           return console.log(err.message);
@@ -18,13 +20,14 @@ async function getProfile(req,res){
         const profile = await api.get(user)
         const repositories = await api.get(repos+'?sort=created&direction=desc')
         const filterRepo = []
-        
+        let result = await getLatest()
+        latestSearch.push(result)
         for(i =0; i< 3;i++){
             if(repositories.data[0].private == false){
                 filterRepo.push(repositories.data[i])   
             }
         }
-        return res.render('profile',{user: profile.data, repos: filterRepo})
+        return res.render('index',{user: profile.data, repos: filterRepo,latest: latestSearch[0]})
     }catch(err){
         return res.redirect('/')
     }
